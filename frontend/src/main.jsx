2375287e4806687ce7da68c4f8c88eb08c9164ea
GabrielAlarcon-nyu <eate.gabrieltomas.alarcon.ca@unifranz.edu.bo>
2026-04-11 22:17:38 -0400

    Clerk en español "notificaciones"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { esES } from '@clerk/localizations'
 
-// Aquí pegas tu Publishable Key de la imagen que me pasaste
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("")
+  throw new Error('')
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={esES}>
       <App />
     </ClerkProvider>
   </React.StrictMode>,
